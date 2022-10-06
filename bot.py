c1f43df5937060dc12caa3220b3e4f03aee4d2c3
Maxime "Teelry" Gouet <maxime.gouet40@gmail.com>
2022-10-06 02:51:27 +0200

    Added leaderboard

--- bot.py
+++ bot.py
@@ -2,14 +2,18 @@
 import discord
 import time
 import io
-
-#The class is where the magic happens. You can define functions that happens when something 
-#is triggered, like receiving a message
+import json
+# The class is where the magic happens. You can define functions that happens when something 
+# is triggered, like receiving a message
 class MyClient(discord.Client):
-    #on_ready happens once when the bot is up and running after boot
+    lb = open("leaderb.json","r")
+    leaderb = json.load(lb)
+    lb.close()
+    # on_ready happens once when the bot is up and running after boot
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-    #on_message happens after each message is received
+        print("bot is ready")
+        # I use json to store the scores of everyone's nose hits
+    # on_message happens after each message is received
     async def on_message(self, message):
         print(message.author)
         t = time.localtime()
@@ -21,6 +25,14 @@ class MyClient(discord.Client):
                 await message.add_reaction("👃")
                 await message.add_reaction("✔️")
                 await message.reply("Félicitations ! Tu gagnes 1 point de :nose:")
+                if message.author.name in self.leaderb:
+                   self.leaderb[message.author.name] += 1
+                else:
+                    self.leaderb[message.author.name] = 1
+                print(self.leaderb)
+                lb = open("leaderb.json","w")
+                json.dump(self.leaderb,lb)
+                lb.close()
 
             if (h-m == 1):
                 await message.add_reaction("👃")
@@ -37,9 +49,17 @@ class MyClient(discord.Client):
         if (message.content == "nezsec"):
             d = str(60-s)
             await message.reply("Il reste "+d+" secondes avant la prochaine minute")
-#Intents are what you allowed your bot to access when you invited it to a discord server via link. You can change it on the website
+        if (message.content == "leaderb"):
+            s = ""
+            for name,score in self.leaderb.items():
+                s += name + " a " + str(score) + " points de nez\n"
+            await message.reply(s)
+
+# Intents are what you allowed your bot to access when you invited it to a discord server 
+# via link. You can change it on the website
 intents = discord.Intents.default()
 intents.message_content = True
+
 client = MyClient(intents=intents)
 #I saved the bot key in another file in order to share this without leaking my private key
 f = open("discordkey","r")
